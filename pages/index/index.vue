7b6c435c987a0f3248379b10d01f1f54577b1b9c
AprilEcho <18813314680@163.com>
2023-06-22 15:14:35 +0800

    update:定义公共组件

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -9,7 +9,15 @@
 			<view class="item">国内</view>
 		</scroll-view>
 		<view class="content">
-			<view class="row">每一行新闻</view>
+			<view class="row">
+				<newsbox></newsbox>
+			</view>
+			<view class="row">
+				<newsbox></newsbox>
+			</view>
+			<view class="row">
+				<newsbox></newsbox>
+			</view>
 		</view>
 	</view>
 </template>
@@ -31,24 +39,35 @@
 </script>
 
 <style lang="scss" scoped>
-.navscroll{
-	height: 100rpx;
+	.navscroll {
+		height: 100rpx;
 		white-space: nowrap;
-	background-color: #F7F8FA;
-	/deep/ ::-webkit-scrollbar {
-		width: 4px !important;
-		height: 1px !important;
-		overflow: auto !important;
-		background: transparent !important;
-		-webkit-appearance: auto !important;
-		display: block;
+		background-color: #F7F8FA;
+
+		/deep/ ::-webkit-scrollbar {
+			width: 4px !important;
+			height: 1px !important;
+			overflow: auto !important;
+			background: transparent !important;
+			-webkit-appearance: auto !important;
+			display: block;
+		}
+
+		.item {
+			font-size: 40rpx;
+			display: inline-block;
+			line-height: 100rpx;
+			padding: 0 30rpx;
+			color: #333;
+		}
 	}
-	.item{
-		font-size: 40rpx;
-		display: inline-block;
-		line-height: 100rpx;
-		padding: 0 30rpx;
-		color: #333;
+
+	.content {
+		padding: 30rpx;
+
+		.row {
+			border-bottom: 1rpx dashed #efefef;
+			padding: 20rpx 0;
+		}
 	}
-}
 </style>
